5b5d23f082be5214c8bdf9d0971d8fc9715fde07
Lili Manrique <54088274+lmanriq@users.noreply.github.com>
2019-12-27 17:49:14 -0700

    pass tests

--- lunchbox.js
+++ lunchbox.js
@@ -1,5 +1,27 @@
 class LunchBox {
+  constructor(box) {
+    this.owner = box.owner;
+    this.material = box.madeOf;
+    this.shape = box.shape;
+    this.color = box.color;
+    this.handle = true;
+    this.snacks = [];
+  }
 
+  acquireSnack(snack){
+    this.snacks.push(snack);
+    snack.isInLunchBox = true;
+  }
+
+  findHealthySnacks() {
+    var healthySnacks = [];
+    for (var i = 0; i < this.snacks.length; i ++) {
+      if (this.snacks[i].healthy === true) {
+        healthySnacks.push(this.snacks[i]);
+      }
+    }
+    return healthySnacks;
+  }
 }
 
 module.exports = LunchBox;
