3662adc91b0676513a1fcc6e85e4c7628209aebd
Lili Manrique <54088274+lmanriq@users.noreply.github.com>
2019-12-27 17:50:03 -0700

    pass tests

--- snack.js
+++ snack.js
@@ -1,5 +1,22 @@
 class Snack {
+  constructor(type) {
+    this.deliciousLevel = 'extra';
+    this.type = type;
+    this.amount = 100;
+    this.isInLunchBox = false;
+    this.cuttingItClose = false;
+    this.healthy = false;
+    if (this.type.includes('Fruit')) {
+      this.healthy = true;
+    }
+  }
 
+  getEaten() {
+    this.amount -= 10;
+    if (this.amount <= 20) {
+      this.cuttingItClose = true;
+    }
+  }
 }
 
 module.exports = Snack;
